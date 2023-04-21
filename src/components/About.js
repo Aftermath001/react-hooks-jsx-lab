7cda7d6e37496e2c8befe79ec38bf6709d020bf7
Aftermath001 <alvinasiachi@gmail.com>
2023-04-21 23:27:36 +0300

    Inline Style

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div id ="About">
+  return <div id ="about">
      <h2>About Me</h2>
     <p>I am the best Developer</p>
     <img src={image} alt="I made this"></img>
