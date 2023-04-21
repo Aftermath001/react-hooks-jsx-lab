d2401348030bb26cc0ee4f82b2b5b1519517ee23
Aftermath001 <alvinasiachi@gmail.com>
2023-04-21 22:40:39 +0300

    Data

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,9 @@ import { image } from "../data/data";
 
 function About() {
   return <div id ="About">
-   
+     <h2>About Me</h2>
+    <p>I am the best Developer</p>
+    <img src={image} alt="I made this"></img>
   </div>;
 }
 
